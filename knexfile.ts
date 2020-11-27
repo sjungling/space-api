621c21f97b8343e7fb086f56f2452817c3e37c20
Scott Jungling <scott.jungling@gmail.com>
2020-11-27 12:54:53 -0800

    fix: netlify issue with reading database ?

--- knexfile.ts
+++ knexfile.ts
@@ -1,17 +1,19 @@
 // Update with your config settings.
 
+import { join } from "path";
+
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./src/apollo.sqlite3",
+      filename: join(__dirname, "src", "apollo.sqlite3"),
     },
     useNullAsDefault: false,
   },
   production: {
     client: "sqlite3",
     connection: {
-      filename: "./dist/apollo.sqlite3",
+      filename: join(__dirname, "dist", "apollo.sqlite3"),
     },
     useNullAsDefault: false,
   },
