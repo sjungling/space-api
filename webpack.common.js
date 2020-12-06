ef8d9e0a197572b8ddd2224e44669b49e1caa352
Scott Jungling <scott.jungling@gmail.com>
2020-12-05 23:48:09 -0800

    feat: add service worker

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,7 @@ const ProgressBarPlugin = require("progress-bar-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin")
   .default;
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -49,6 +50,12 @@ module.exports = {
       "process.env.NODE_ENV": JSON.stringify(
         process.env.NODE_ENV || "development"
       ),
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
     }),
 
     new BundleAnalyzerPlugin({
