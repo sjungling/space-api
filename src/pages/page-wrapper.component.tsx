61b1a8428c43d455594f6ab611bc27038ea0de67
Scott Jungling <scott.jungling@gmail.com>
2020-12-15 23:17:43 -0800

    fix: missing page-wrapper and reset yarn.lock

---
+++ src/pages/page-wrapper.component.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react";
+import useMetaTags from "react-metatags-hook";
+
+export type PageWrapperProps = {
+  title?: string;
+  description?: string;
+};
+export const PageWrapper: FunctionComponent<PageWrapperProps> = ({
+  title,
+  description,
+  children,
+}) => {
+  const BASE_TITLE = "SpaceAPI.dev";
+  useMetaTags({
+    title: title ? `${[title]} - ${BASE_TITLE}` : BASE_TITLE,
+    description,
+  });
+  return <div>{children}</div>;
+};
